eb3d9d066b1462682fde7a379c3e26badc9e5164
Jheezzy <horlajidey.olajide3@gmail.com>
2023-08-07 23:20:52 +0100

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app_fixed/main_body.dart';
 
 void main() {
-  runApp(const TodoApp());
+  runApp(
+    const ProviderScope(
+      child: TodoApp(),
+    ),
+  );
 }
 
 class TodoApp extends StatelessWidget {
